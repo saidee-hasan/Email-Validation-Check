e6c77f0a66384feabe407ffe9cb9447003865d8c
saideeWD <mdsaideehasan@gmail.com>
2024-07-11 20:54:10 +0600

    valid

---
+++ main.js
@@ -0,0 +1,11 @@
+
+if(email.matches(pattern)){
+    
+
+}else{
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML =" Pleasae Enter Your Valid Email Address .";
+    text.style.color ="#00ff00";
+
+}
