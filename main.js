8e3cf639b1b0c391b195a347c8753b1a4a260f1e
saideeWD <mdsaideehasan@gmail.com>
2024-07-11 21:23:13 +0600

    successfully

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-
-if(email.matches(pattern)){
-    
-
+        
+function validation (){
+    let form = document.getElementById("form");
+    let email = document.getElementById("email").value;
+    let text = document.getElementById("text");
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+if(email.match(pattern)){
+   form.classList.add("valid");
+   form.classList.remove("invalid");
+    text.innerHTML ="Your Email Address in Valid.";
+    text.style.color ="#00ff00";
 }else{
     form.classList.remove("valid");
-    form.classList.add("invalid");
-    text.innerHTML =" Pleasae Enter Your Valid Email Address .";
+   form.classList.add("invalid");
+    text.innerHTML =" Please Enter Valid Email Address .";
+    text.style.color ="#ff0000";
+}
+    
+if(email == ""){
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML =" ";
     text.style.color ="#00ff00";
+
+}
 
 }
